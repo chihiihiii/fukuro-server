5a157922b23254683b358acc4f717f2b1db78544
chihiihiii <lechi98.ct@gmail.com>
2021-11-17 14:12:15 +0700

    update comment + reset pass

---
+++ app/migrations/20211117060433-create-comment.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      customerId: {
+        type: Sequelize.INTEGER,
+        field: 'customer_id',
+        references: {
+          model: {
+            tableName: 'Customers',
+          },
+          key: 'id'
+        },
+      },
+      blogId: {
+        type: Sequelize.INTEGER,
+        field: 'blog_id',
+        references: {
+          model: {
+            tableName: 'Blogs',
+          },
+          key: 'id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
---
+++ app/models/comment.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Comment.init({
+    content: {
+      type: DataTypes.TEXT
+    },
+    status: {
+      type: DataTypes.STRING
+    },
+    customerId: {
+      type: DataTypes.INTEGER,
+      field: 'customer_id',
+      references: {
+        model: {
+          tableName: 'Customers',
+        },
+        key: 'id'
+      },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      field: 'blog_id',
+      references: {
+        model: {
+          tableName: 'Blogs',
+        },
+        key: 'id'
+      },
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
